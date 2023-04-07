4e566fd4308e671b39623e3789be00cf5db6530b
pipecraft <pipecraft.net@gmail.com>
2023-04-07 10:32:02 +0800

    First commit of v2ex.min

---
+++ build/userscript-prod/v2ex.min.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name                 v2ex.min - V2EX 极简风格
+// @name:zh-CN           v2ex.min - V2EX 极简风格
+// @namespace            https://github.com/v2hot/v2ex.min
+// @homepageURL          https://github.com/v2hot/v2ex.min#readme
+// @supportURL           https://github.com/v2hot/v2ex.min/issues
+// @version              0.0.1
+// @description          V2EX 极简风格，扁平化 UI，导航栏顶部固定，去除背景图片，支持黑暗模式，去除广告，去除不必要元素，支持隐藏头像。
+// @description:zh-CN    V2EX 极简风格，扁平化 UI，导航栏顶部固定，去除背景图片，支持黑暗模式，去除广告，去除不必要元素，支持隐藏头像。
+// @icon                 https://www.v2ex.com/favicon.ico
+// @author               Pipecraft
+// @license              MIT
+// @match                https://*/*
+// @match                http://*/*
+// ==/UserScript==
+//
+;(() => {
+  "use strict"
+})()
